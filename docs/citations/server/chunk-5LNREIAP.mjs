8c4d5d36bdaba02034ebaec609aa4c3736dcb4a4
Rebolon <richard.tribes@gmail.com>
2023-11-20 21:01:27 +0100

    one new cite, and latest ng minor update

---
+++ docs/citations/server/chunk-5LNREIAP.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{A as s,U as i,f as r}from"./chunk-XUIXZ2OS.mjs";var n=(()=>{let e=class e{constructor(){this.refresh=new r(!1),this.refresh$=this.refresh.asObservable().pipe(s(250))}click(){this.refresh.next(!0)}};e.\u0275fac=function(o){return new(o||e)},e.\u0275prov=i({token:e,factory:e.\u0275fac});let t=e;return t})();export{n as a};
